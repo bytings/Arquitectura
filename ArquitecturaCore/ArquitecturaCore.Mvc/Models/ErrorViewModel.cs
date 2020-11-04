62a5cff145a7654921618713134a5317cab9069f
Daniel Mendoza <el_chino07@hotmail.com>
2020-11-04 08:16:14 -0300

    initial commit de arquitecturaCore.Mvc(Presentation), se cambio linea en startup.cs para que el correo no sea validado

---
+++ ArquitecturaCore/ArquitecturaCore.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ArquitecturaCore.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
