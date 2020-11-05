dd6b983bce784c2c2e049f9feba872afb967d217
Daniel Mendoza <el_chino07@hotmail.com>
2020-11-05 09:22:29 -0300

    Configure Presentation Layer for Dependency Container

---
+++ ArquitecturaCore/ArquitecturaCore.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using ArquitecturaCore.Application.Interfaces;
+using ArquitecturaCore.Application.Services;
+using ArquitecturaCore.Domain.Interfaces;
+using ArquitecturaCore.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArquitecturaCore.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            // Infra.Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
--- ArquitecturaCore/ArquitecturaCore.Mvc/Startup.cs
+++ ArquitecturaCore/ArquitecturaCore.Mvc/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ArquitecturaCore.Infra.Data.Context;
+using ArquitecturaCore.Infra.IoC;
 
 namespace ArquitecturaCore.Mvc
 {
@@ -41,6 +42,7 @@ namespace ArquitecturaCore.Mvc
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +74,11 @@ namespace ArquitecturaCore.Mvc
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
