a5e35e615986a233e22b4b63ce6e2187c3b1da62
Daniel Mendoza <el_chino07@hotmail.com>
2020-11-05 08:47:57 -0300

    Commit Service and repository Interfaces, CourseViewModel

---
+++ ArquitecturaCore/ArquitecturaCore.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using ArquitecturaCore.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArquitecturaCore.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
---
+++ ArquitecturaCore/ArquitecturaCore.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using ArquitecturaCore.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArquitecturaCore.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
