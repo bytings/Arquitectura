ccc646dff7abc2463674367436f40c26579d77ff
Daniel Mendoza <el_chino07@hotmail.com>
2020-11-06 15:16:30 -0300

    Commit changes for presentation Course COntroller, views, styling, authorization

---
+++ ArquitecturaCore/ArquitecturaCore.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArquitecturaCore.Application.Interfaces;
+using ArquitecturaCore.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArquitecturaCore.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
