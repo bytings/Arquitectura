14fc7a08af34785f6eb758d9920b37bc1a1438bc
Daniel Mendoza <el_chino07@hotmail.com>
2020-11-06 15:57:36 -0300

    Adding Domain.Core and Implementation of InMemoryBus

---
+++ ArquitecturaCore/ArquitecturaCore.Domain.Core/Commands/Command.cs
@@ -0,0 +1,18 @@
+﻿using ArquitecturaCore.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ArquitecturaCore.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
--- ArquitecturaCore/ArquitecturaCore.Infra.Bus/InMemoryBus.cs
+++ ArquitecturaCore/ArquitecturaCore.Infra.Bus/InMemoryBus.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using ArquitecturaCore.Domain.Core.Bus;
+using ArquitecturaCore.Domain.Core.Commands;
+using MediatR;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ArquitecturaCore.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
+        private readonly IMediator _mediator;
 
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T: Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
